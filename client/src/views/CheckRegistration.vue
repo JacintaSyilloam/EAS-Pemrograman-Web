a9dc3bc4e2477a441db419f1b7344e93e773d083
JacintaSyilloam <jacintasyilloam@gmail.com>
2023-12-14 17:52:47 +0700

    fix search email

--- client/src/views/CheckRegistration.vue
+++ client/src/views/CheckRegistration.vue
@@ -4,51 +4,58 @@
         <form @submit.prevent="check">
             <p>Email:</p>
             <input v-model="email" type="email" placeholder="email" />
-            <div v-for="(stat, idx) in status" :key="idx">
-                {{ stat.status }}
+            <div>
+                {{ account?.status }}
             </div>
             <br>
             <br>
             <button type="submit">Check</button>
         </form>
-
-        debug syid
-        <div v-for="account in accounts" :key="account.id">
-            {{ account.name }}
-            {{ account.email }}
-            {{ account.institution }}
-            {{ account.date }}
-            {{ account.status }}
-        </div>
-
     </main>
 </template>
 
 <script>
+import qs from 'qs'
+
 export default {
     data() {
         return {
             email: '',
-            status: []
+            account: {},
         }
     },
     methods: {
         async check() {
+            const query = {
+                email: {
+                    equals: this.email
+                }
+            }
+
+            const stringifiedQuery = qs.stringify(
+                {
+                    where: query,
+                },
+                { addQueryPrefix: true },
+            );
+
             try {
-                const response = await fetch('http://localhost:3000/api/accounts?where[email][equals]=${this.email}', {
+                const response = await fetch(`http://localhost:3000/api/accounts${stringifiedQuery}`, {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json'
                     }
                 })
+
                 if (!response.ok) {
                     const errorMsg = (await response.json())?.errors[0].message
                     throw new Error(errorMsg)
-                }   
+                }
 
                 const data = await response.json();
-                this.status = data.docs;
-                console.log(this.status);
+                console.log(data);
+                this.account = data.docs[0];
+                
             } catch (error) {
                 console.error(error.message)
             }
